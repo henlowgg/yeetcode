1e76f465d361c83934e36b4e594a5e3740da0c9c
Jack Ault <henlowgg@outlook.com>
2023-12-17 21:31:08 -0500

    solution for Sleep

--- 30 Days of JavaScript/Promises and Time/2621. Sleep/Sleep.js
+++ 30 Days of JavaScript/Promises and Time/2621. Sleep/Sleep.js
@@ -3,10 +3,12 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    
+    return new Promise(resolve => setTimeout(resolve, millis))
 }
 
 /** 
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
+
+// return a promise resolve, setting the timeout to (resolve, millis)
