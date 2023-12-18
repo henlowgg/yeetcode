9b6180ee64059af4ef0a0173aa0061bf3e025185
Jack Ault <henlowgg@outlook.com>
2023-12-17 21:27:47 -0500

    solution for 2723 leetcode add two promises

--- 30 Days of JavaScript/Promises and Time/2723. Add Two Promises/AddTwoPromises.js
+++ 30 Days of JavaScript/Promises and Time/2723. Add Two Promises/AddTwoPromises.js
@@ -4,10 +4,20 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    
+    let [val1, val2] = await Promise.all([promise1, promise2]);
+    return val1 + val2;
 };
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
  */
+
+
+// use the Promise.all() method and await 
+// waiting for both promises to resolve
+
+// once resolved the values are retrieved and stored in the variables val1 and val2, then a new promise is returned with the sum of val1 and val2
+
+
+// Time: O(max(promise1 time, promise2 time)) which just means the time complexity is determined by the longest running promise since Promise.all() method waits for both promises to resolve concurrently
