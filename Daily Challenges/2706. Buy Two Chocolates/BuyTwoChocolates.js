5589256902b201f8eebcc5596b131850a9b39fb0
Jack Ault <henlowgg@outlook.com>
2023-12-19 20:50:11 -0500

    solution

--- Daily Challenges/2706. Buy Two Chocolates/BuyTwoChocolates.js
+++ Daily Challenges/2706. Buy Two Chocolates/BuyTwoChocolates.js
@@ -1,3 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
 var buyChoco = function(prices, money) {
+    let min = Infinity;
+    let n = prices.length;
 
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let cost = prices[i] + prices[j];
+
+            if (cost < min) {
+                min = cost;
+            }
+        }
+    }
+    if (min <= money) {
+        return money - min;
+    } else {
+        return money;
+    }
 };
