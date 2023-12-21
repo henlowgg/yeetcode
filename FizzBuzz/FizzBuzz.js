09a6812cc6018a831bb1e818e5da144196b378b2
Jack Ault <henlowgg@outlook.com>
2023-12-21 10:54:03 -0500

    more more more

--- FizzBuzz/FizzBuzz.js
+++ FizzBuzz/FizzBuzz.js
@@ -150,3 +150,31 @@ const okGoingToShower = (n) => {
         };
     return showeringNow;
 };
+
+var fizzbuzzy = function(n) {
+    let result = []
+        for (let i = 1; i <= n; i++) {
+            if (i % 3 === 0 && i % 5 === 0)
+                result.push("FizzBuzz")
+            else if (i % 3 === 0)
+                result.push("Fizz")
+            else if (i % 5 === 0)
+                result.push("Buzz")
+            else
+                result.push(i + "")
+        }
+        return result;
+}
+
+const frazzBruzz = (n) => {
+    let frizzle = [];
+    for (let i = 1; i <= n; i++) {
+        let brizzle = "";
+
+        if (i % 3 === 0) brizzle.push += "Fizz";
+        if (i % 5 === 0) brizzle.push += "Buzz";
+        if (brizzle === "") brizzle += i;
+            frizzle.push(brizzle);
+    };
+    return frizzle;
+}
