629b1573cee22c1e2ea4be442fc11984421afc45
Jack Ault <henlowgg@outlook.com>
2023-12-21 09:09:37 -0500

    solution in two ways, one way by arrow function with concat, and another by simple else if statements

--- FizzBuzz/FizzBuzz.js
+++ FizzBuzz/FizzBuzz.js
@@ -1,5 +1,45 @@
 /* First solution */
 
-var fizzBuzz = function (n) {
+// change the initial function into an arrow func
 
+const fizzBuzz = (n) => {
+
+    // declare an empty array
+    let result = [];
+
+    // write a for loop to iterate over the arrays 
+    for (let i = 1; i <= n; i++) {
+        // going to concatenate the strings with these functions
+        let stringResult = "";
+        if (i % 3 === 0) stringResult += "Fizz";
+        if (i % 5 === 0) stringResult += "Buzz";
+        // concats the strings if both are true
+        
+        if (stringResult === "") stringResult += i;
+        result.push(stringResult);
+    }
+    return result;
+};
+
+
+/* Second solution, standard if elseif statements to check if indexed string is true or false */
+var fizzyBuzz = function(n) {
+    arr = []
+
+    for (let i = 1; i <= n; i++) {
+        // check for fizzbuzz first since that takes both arguments %3 and %5
+        if (i % 3 === 0 && i % 5 === 0)
+            // pushes FizzBuzz to the arr if both i % 3 and i % 5 are true
+            arr.push("FizzBuzz")
+        else if (i % 3 === 0)
+            // pushes Fizz to the arr if i % 3 is true
+            arr.push("Fizz")
+        else if (i % 5 === 0)
+            // pushes Buzz to the arr if i % 5 is true
+            arr.push("Buzz")
+        else
+            // if none of those conditions are met, it will return i as a string
+            arr.push(i + "")
+    }
+    return arr
 };
