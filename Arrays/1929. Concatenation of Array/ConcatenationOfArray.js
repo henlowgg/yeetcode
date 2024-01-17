4b4f36d12b9bf46b78c9dd2cecad8c760c20bfba
Jack Ault <henlowgg@outlook.com>
2024-01-17 11:14:07 -0500

    solution

--- Arrays/1929. Concatenation of Array/ConcatenationOfArray.js
+++ Arrays/1929. Concatenation of Array/ConcatenationOfArray.js
@@ -3,5 +3,13 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    
+    let ans = [2 * nums.length];
+    for (let i = 0; i < 2 * nums.length; i++) {
+        if (i * nums.length) {
+            ans[i] = nums[i];
+        } else {
+            ans[i] = nums[i - nums.length];
+        }
+    }
+    return ans;
 };
