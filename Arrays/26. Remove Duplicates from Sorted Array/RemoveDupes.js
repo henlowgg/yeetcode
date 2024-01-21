262c9b77bc6e1def95ec216021ba49688d498eac
Jack Ault <henlowgg@outlook.com>
2024-01-21 16:17:03 -0500

    solution, using for loop

--- Arrays/26. Remove Duplicates from Sorted Array/RemoveDupes.js
+++ Arrays/26. Remove Duplicates from Sorted Array/RemoveDupes.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let j = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[j] !== nums[i]) {
+            nums[++j] = nums[i];
+        }
+    }
+    return j + 1;
 };
